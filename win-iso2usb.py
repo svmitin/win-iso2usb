f3b85a95fd72218ce8c32dfda4198172e7321b03
Sergey Mitin <svmitin@outlook.com>
2023-03-12 14:49:42 +0600

    fixes

--- win-iso2usb.py
+++ win-iso2usb.py
@@ -7,32 +7,38 @@ def run(command: str) -> str:
     output = stream.read()
     return output
 
-def get_usb_device_path():
+def get_usb_device_path() -> str:
     disks = run('diskutil list').split('\n')
     for line in disks:
         if ' (external, physical)' in line:
             device_path, _ = line.split(' (external, physical)')
             return device_path
+    return 'not found'
 
 DEVICE = input(f'USB device path ({get_usb_device_path()}): ') or get_usb_device_path()
 WINDOWS_ISO_PATH = input('ISO file path (~/Downloads/LB2CFRE_EN_DVD.iso): ') or '~/Downloads/LB2CFRE_EN_DVD.iso'
+
+agree = input('All files on your usb-flash will be destroyed. Press "Y" if you shure: ').lower()
+if agree != 'y':
+    print('Operation not permitted')
+    exit()
 
 erase = run(f'diskutil eraseDisk MS-DOS WININST MBR {DEVICE}')
 mount_iso = run(f'hdiutil mount {WINDOWS_ISO_PATH}')
 _, mount_iso_path = mount_iso.replace('\n', '').split('/Volumes/')
 
-print('copying files. part 1')
+print('Copying files. part 1')
 run(f'rsync -avh --progress --exclude=sources/install.wim /Volumes/{mount_iso_path}/ /Volumes/WININST')
 
-print('installing dependecies')
+print('Installing dependecies')
 run('xcode-select --install')
 run('brew install wimlib')
 
-print('copying files. part 2')
+print('Copying files. part 2')
 run(f'wimlib-imagex split /Volumes/{mount_iso_path}/sources/install.wim /Volumes/WININST/sources/install.swm 3000')
 
-print('unmounting iso')
+print('Unmounting iso')
 run(f'umount /Volumes/{mount_iso_path}')
 
-print('done')
+print('Done')
 
